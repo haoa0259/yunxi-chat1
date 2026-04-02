c3ab1d0020ea7065b5c93bd70ccc9389698f88fe
haoa0259 <haoa0259@gmail.com>
2026-04-03 03:58:54 +0800

    Add Tailwind CSS configuration file

---
+++ prisma/frontend/frontend/frontend/frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6B35',
+        secondary: '#E8C28A',
+      },
+    },
+  },
+  plugins: [],
+}
